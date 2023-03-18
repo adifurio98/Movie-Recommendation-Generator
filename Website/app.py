d5d1df6a7059fdb79b4590bdde80ac218dde8857
LWKYDTWSH <106898202+LWKYDTWSH@users.noreply.github.com>
2023-03-18 14:38:53 -0500

    Level 1 app

---
+++ Website/app.py
@@ -0,0 +1,50 @@
+from flask import Flask, render_template, redirect
+
+# Create an instance of Flask
+app = Flask(__name__)
+app.config['SEND_FILE_MAX_AGE_DEFAULT'] = 0
+
+# Route to render index.html template using data from Mongo
+@app.route("/")
+def home():
+    # Return template and data
+    return render_template("index.html")
+
+@app.route("/about_us")
+def about_us():
+    # Return template and data
+    return render_template("about_us.html")
+
+@app.route("/works_cited")
+def works_cited():
+    # Return template and data
+    return render_template("works_cited.html")
+
+@app.route("/machine_learning")
+def machine_learning():
+    # Return template and data
+    return render_template("machine_learning.html")
+
+@app.route("/source_data")
+def source_data():
+    # Return template and data
+    return render_template("source_data.html")
+
+
+#############################################################
+
+@app.after_request
+def add_header(r):
+    """
+    Add headers to both force latest IE rendering engine or Chrome Frame,
+    and also to cache the rendered page for 10 minutes.
+    """
+    r.headers['X-UA-Compatible'] = 'IE=Edge,chrome=1'
+    r.headers["Cache-Control"] = "no-cache, no-store, must-revalidate, public, max-age=0"
+    r.headers["Pragma"] = "no-cache"
+    r.headers["Expires"] = "0"
+    return r
+
+#main
+if __name__ == "__main__":
+    app.run(debug=True)
